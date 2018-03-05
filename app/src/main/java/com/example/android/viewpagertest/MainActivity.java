f7fdc46169da75a74029c8e9fdafab6ce4bf9901
jerard <jerardangelo@hotmail.com>
2018-03-05 15:54:14 -0800

    Initial commit

---
+++ app/src/main/java/com/example/android/viewpagertest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.viewpagertest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void start(View view){
+        Intent i = (new Intent(MainActivity.this, ScreenSlidePagerActivity.class));
+        startActivity(i);
+
+    }
+}
---
+++ app/src/test/java/com/example/android/viewpagertest/ScreenSlidePagerActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.android.viewpagertest;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jerard on 2/23/18.
+ */
+public class ScreenSlidePagerActivityTest {
+
+}
